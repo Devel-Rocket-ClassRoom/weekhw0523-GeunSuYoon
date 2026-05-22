8b1b172240048b96b6b9b1bb3309912778ed8d5c
EulSu <146644182+GeunSuYoon@users.noreply.github.com>
2026-05-22 17:35:50 +0900

    정리

--- Unreal10thWeekendHomework/Unreal10thWeekendHomework/Homework0523To0525/Homework.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <iostream>
-#include <random>
-
-void	Homework02_Run(void);
-void	Homework03_Run(void);
-void	Homework04_Run(void);
-void	Homework05_Run(void);
--- Unreal10thWeekendHomework/Unreal10thWeekendHomework/Unreal10thWeekendHomework.cpp
+++
@@ -1,7 +0,0 @@
-#include "Homework0523To0525/Homework.h"
-
-int main()
-{
-	srand(time(0));
-	Homework03_Run();
-}
