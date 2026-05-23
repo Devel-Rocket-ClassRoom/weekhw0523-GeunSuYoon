bf48094f73907b5719f0ae273f4b7e99fe9d90ef
EulSu <146644182+GeunSuYoon@users.noreply.github.com>
2026-05-23 13:51:21 +0900

    feat: Add All Homework Funcs to main

--- Unreal10thWeekendHomework/Unreal10thWeekendHomework.cpp
+++ Unreal10thWeekendHomework/Unreal10thWeekendHomework.cpp
@@ -1,7 +1,10 @@
-#include "HomeworkCode/Homework.h"
+﻿#include "HomeworkCode/Homework.h"
 
 int main()
 {
 	srand(time(0));
+	Homework02_Run();
 	Homework03_Run();
+	Homework04_Run();
+	Homework05_Run();
 }
