a1575c07efd8591e77cda008555acb6942da873d
EulSu <146644182+GeunSuYoon@users.noreply.github.com>
2026-05-23 10:17:34 +0900

    chore: Add print for UX

--- Unreal10thWeekendHomework/HomeworkCode/Homework02.cpp
+++ Unreal10thWeekendHomework/HomeworkCode/Homework02.cpp
@@ -1,4 +1,4 @@
-#include "./Homework.h"
+﻿#include "./Homework.h"
 
 void	Homework02_Run(void);
 
@@ -8,11 +8,12 @@ void	Homework02_Run(void)
 	int	InputVal = 0;
 
 	printf("Homework02_Run\n");
+	printf("숫자를 입력해주세요 : ");
 	std::cin >> InputVal;
 	while (InputVal)
 	{
 		SumValue += InputVal % 10;
 		InputVal /= 10;
 	}
-	printf("입력한 값의 총 합은 [%d]입니다.\n", SumValue);
+	printf("입력한 값의 자릿수 합은 [%d]입니다.\n", SumValue);
 }
